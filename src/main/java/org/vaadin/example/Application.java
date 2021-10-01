80cf622d8d46c96d8d3010785b7d94ec09077679
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-10-01 14:04:16 +0300

    Update to match 14.7.1 skeleton

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -1,4 +1,4 @@
-package org.vaadin.marcus.spring;
+package org.vaadin.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
